4c34e6446b294e5bb1e6b1f9e2d9154d4a7e842d
kumip12 <pkumi559@gmail.com>
2016-02-10 23:27:01 +0000

    wok

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    
+     $('onclick').click(function() {
+         
+       alert('sanity');
+         
+     });
+    
+});
+
+
+// var color = ["red","yellow","green","white","purpel","pink"]
